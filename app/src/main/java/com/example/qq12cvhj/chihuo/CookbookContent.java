4e7c78ab477f715905ab604ae4611e954e1ce8df
jcwayd <qq12cvhj@163.com>
2018-03-20 20:33:40 +0800

    引入expandable列表在菜谱部分使用

--- app/src/main/java/com/example/qq12cvhj/chihuo/CookbookContent.java
+++ app/src/main/java/com/example/qq12cvhj/chihuo/CookbookContent.java
@@ -4,9 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SimpleItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.h6ah4i.android.widget.advrecyclerview.expandable.RecyclerViewExpandableItemManager;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by qq12cvhj on 2018/3/18.
@@ -16,7 +27,139 @@ public class CookbookContent extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.cookbook_content,null,false);
+        View view = inflater.inflate(R.layout.cookbook_content, null, false);
+
         return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        RecyclerView recyclerView = getActivity().findViewById(R.id.recycler_cookbook);
+        RecyclerViewExpandableItemManager expMgr = new RecyclerViewExpandableItemManager(null);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(expMgr.createWrappedAdapter(new MyAdapter()));
+        ((SimpleItemAnimator) recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
+        expMgr.attachRecyclerView(recyclerView);
+        super.onStart();
+    }
+
+    static abstract class MyBaseItem {
+        public final long id;
+        public final String text;
+
+        public MyBaseItem(long id, String text) {
+            this.id = id;
+            this.text = text;
+        }
+    }
+
+    static class MyGroupItem extends MyBaseItem {
+        public final List<MyChildItem> children;
+
+        public MyGroupItem(long id, String text) {
+            super(id, text);
+            children = new ArrayList<>();
+        }
+    }
+
+    static class MyChildItem extends MyBaseItem {
+        public MyChildItem(long id, String text) {
+            super(id, text);
+        }
+    }
+
+    static abstract class MyBaseViewHolder extends AbstractExpandableItemViewHolder {
+        TextView textView;
+
+        public MyBaseViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(android.R.id.text1);
+        }
+    }
+
+    static class MyGroupViewHolder extends MyBaseViewHolder {
+        public MyGroupViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    static class MyChildViewHolder extends MyBaseViewHolder {
+        public MyChildViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    static class MyAdapter extends AbstractExpandableItemAdapter<MyGroupViewHolder, MyChildViewHolder> {
+        List<MyGroupItem> mItems;
+
+        public MyAdapter() {
+            setHasStableIds(true); // this is required for expandable feature.
+
+            mItems = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                MyGroupItem group = new MyGroupItem(i, "GROUP " + i);
+                for (int j = 0; j < 5; j++) {
+                    group.children.add(new MyChildItem(j, "child " + j));
+                }
+                mItems.add(group);
+            }
+        }
+
+        @Override
+        public int getGroupCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public int getChildCount(int groupPosition) {
+            return mItems.get(groupPosition).children.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            // This method need to return unique value within all group items.
+            return mItems.get(groupPosition).id;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            // This method need to return unique value within the group.
+            return mItems.get(groupPosition).children.get(childPosition).id;
+        }
+
+        @Override
+        public MyGroupViewHolder onCreateGroupViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_group_item_for_expandable_minimal, parent, false);
+            return new MyGroupViewHolder(v);
+        }
+
+        @Override
+        public MyChildViewHolder onCreateChildViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_child_item_for_expandable_minimal, parent, false);
+            return new MyChildViewHolder(v);
+        }
+
+        @Override
+        public void onBindGroupViewHolder(MyGroupViewHolder holder, int groupPosition, int viewType) {
+            MyGroupItem group = mItems.get(groupPosition);
+            holder.textView.setText(group.text);
+        }
+
+        @Override
+        public void onBindChildViewHolder(MyChildViewHolder holder, int groupPosition, int childPosition, int viewType) {
+            MyChildItem child = mItems.get(groupPosition).children.get(childPosition);
+            holder.textView.setText(child.text);
+        }
+
+        @Override
+        public boolean onCheckCanExpandOrCollapseGroup(MyGroupViewHolder holder, int groupPosition, int x, int y, boolean expand) {
+            return true;
+        }
     }
 }
