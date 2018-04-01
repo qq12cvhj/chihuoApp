169dfd749f015ed00c21cadfc36b4e24a5e5d14d
jcwayd <qq12cvhj@163.com>
2018-04-01 16:41:25 +0800

    优化菜品添加功能。

--- app/src/main/java/com/example/qq12cvhj/chihuo/addNewFoodActivity.java
+++ app/src/main/java/com/example/qq12cvhj/chihuo/addNewFoodActivity.java
@@ -6,15 +6,20 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 
-public class addNewFoodActivity extends AppCompatActivity {
+public class addNewFoodActivity extends AppCompatActivity implements View.OnClickListener {
     WebView cookbookEditWebView;
+    private Button addFoodSubmitBtn;
     private ValueCallback<Uri> uploadMessage;
     private ValueCallback<Uri[]> uploadMessageAboveL;
     private final static int FILE_CHOOSER_RESULT_CODE = 10000;
@@ -55,6 +60,8 @@ public class addNewFoodActivity extends AppCompatActivity {
             }
         });
         cookbookEditWebView.loadUrl(commonInfo.httpUrl("editCookbook"));
+        addFoodSubmitBtn = findViewById(R.id.addFoodSubmitBtn);
+        addFoodSubmitBtn.setOnClickListener(this);
     }
     private void openImageChooserActivity() {
         Intent i = new Intent(Intent.ACTION_GET_CONTENT);
@@ -100,5 +107,22 @@ public class addNewFoodActivity extends AppCompatActivity {
         }
         uploadMessageAboveL.onReceiveValue(results);
         uploadMessageAboveL = null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.addFoodSubmitBtn:
+                cookbookEditWebView.evaluateJavascript("javaScript:callSubmit()",new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        //此处为 js 返回的结果
+                        Log.d("test",value);
+                    }
+                });
+
+                break;
+        }
     }
 }
