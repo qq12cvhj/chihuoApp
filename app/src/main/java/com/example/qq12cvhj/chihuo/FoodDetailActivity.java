62f2d804de2ec30d34a258ee41c1a1cc31d08b2c
jcwayd <qq12cvhj@163.com>
2018-04-06 23:51:28 +0800

    添加菜品收藏功能,后台同步。

--- app/src/main/java/com/example/qq12cvhj/chihuo/FoodDetailActivity.java
+++ app/src/main/java/com/example/qq12cvhj/chihuo/FoodDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.qq12cvhj.chihuo;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ClipData;
@@ -9,23 +10,34 @@ import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.util.Log;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-public class FoodDetailActivity extends AppCompatActivity {
+public class FoodDetailActivity extends AppCompatActivity implements View.OnClickListener {
     WebView getFoodInfoWebView;
+    Button starBtn;
+    int trFoodid;
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_datail);
         Intent intent = getIntent();
-        int trFoodid = intent.getIntExtra("trFoodid",-1);
+        trFoodid = intent.getIntExtra("trFoodid",-1);
         String trFoodName = intent.getStringExtra("trFoodName");
         /**没得到传来的值，出错，关闭activity*/
         if(trFoodid==-1){
@@ -44,6 +56,8 @@ public class FoodDetailActivity extends AppCompatActivity {
                 actionBar.hide();
                 //actionBar.setTitle(trFoodName);
             }
+            starBtn = findViewById(R.id.starBtn);
+            starBtn.setOnClickListener(this);
             getFoodInfoWebView = findViewById(R.id.getFoodInfoWebView);
             WebSettings settings = getFoodInfoWebView.getSettings();
             settings.setUseWideViewPort(true);
@@ -55,10 +69,103 @@ public class FoodDetailActivity extends AppCompatActivity {
             getFoodInfoWebView.setInitialScale(200);*/
             getFoodInfoWebView.setWebChromeClient(new WebChromeClient(){});
             getFoodInfoWebView.loadUrl(commonInfo.httpUrl("getFoodInfo"+trFoodid));
+
+            if(!commonInfo.loginStatus){
+                starBtn.setText("登录收藏");
+            }else{
+                if(starStatus(commonInfo.currentUserId,trFoodid)){
+                    starBtn.setText("取消收藏");
+                }else{
+                    starBtn.setText("点击收藏");
+                }
+
+            }
         }
     }
 
+    private void starOrCancel(final int foodId, final int userId){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(commonInfo.httpUrl("starOrCancel"+foodId+"/"+userId))
+                            .build();
+                    client.newCall(request).execute();
+
+                }catch (Exception e){
+
+                }
+            }
+        }).run();
+
+    }
+
+    private int getStarCount(int foodId){
+        final int[] responseData = new int[1];
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(commonInfo.httpUrl("getStarCount"+ trFoodid))
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    responseData[0] = Integer.parseInt(response.body().string());
+                    Log.d("responseData", String.valueOf(responseData[0]));
+                }catch(Exception e){
+                    e.printStackTrace();
+                    responseData[0] = 0;
+                }
+            }
+        }).run();
+        return responseData[0];
+    }
+    private boolean starStatus(final int userId, final int foodId){
+        final boolean[] status = new boolean[1];
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(commonInfo.httpUrl("starStatus"+foodId+"/"+userId))
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    int responseData = Integer.parseInt(response.body().string());
+                    if(responseData == 1) status[0] = true;
+                    else status[0] = false;
+                }catch(Exception e){
+                    e.printStackTrace();
+                    status[0] = false;
+                }
+            }
+        }).run();
+        return status[0];
+    }
 
 
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.starBtn:
+                if(starBtn.getText().equals("点击收藏")){
+                    starOrCancel(trFoodid,commonInfo.currentUserId);
+                    starBtn.setText("取消收藏");
+                }else if(starBtn.getText().equals("取消收藏")){
+                    starOrCancel(trFoodid,commonInfo.currentUserId);
+                    starBtn.setText("点击收藏");
+                }else{
+                    toastShow("登录后才能收藏哈！！");
+                }
+                break;
+        }
 
+    }
+    private void toastShow(String str){
+        Toast.makeText(this,str, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/qq12cvhj/chihuo/addNewFoodActivity.java
+++ app/src/main/java/com/example/qq12cvhj/chihuo/addNewFoodActivity.java
@@ -9,7 +9,6 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -17,7 +16,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 
-import cn.pedant.SweetAlert.SweetAlertDialog;
+
 
 public class addNewFoodActivity extends AppCompatActivity implements View.OnClickListener {
     WebView cookbookEditWebView;
