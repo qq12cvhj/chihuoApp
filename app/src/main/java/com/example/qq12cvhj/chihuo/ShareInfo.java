5e620f4b3c6ca153c744c48a82fb6148be8cf7fb
jcwayd <qq12cvhj@163.com>
2018-04-10 21:10:07 +0800

    增加share类信息，添加一个shareList的列表activity。

---
+++ app/src/main/java/com/example/qq12cvhj/chihuo/ShareInfo.java
@@ -0,0 +1,27 @@
+package com.example.qq12cvhj.chihuo;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by qq12cvhj on 2018/4/10.
+ */
+
+public class ShareInfo {
+    public int shareId;
+    public int shareTitle;
+    public int shareAuthor;
+    public Date pubDate;
+    @SuppressLint("SimpleDateFormat")
+    public SimpleDateFormat sdft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+    public String pubTimeStr = sdft.format(pubDate);
+    public ShareInfo(int shareId, int shareTitle, int shareAuthor, String pubTimeStr) {
+        this.shareId = shareId;
+        this.shareTitle = shareTitle;
+        this.shareAuthor = shareAuthor;
+        this.pubTimeStr = pubTimeStr ;
+    }
+}
---
+++ app/src/main/java/com/example/qq12cvhj/chihuo/ShareListActivity.java
@@ -0,0 +1,13 @@
+package com.example.qq12cvhj.chihuo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_list);
+    }
+}
